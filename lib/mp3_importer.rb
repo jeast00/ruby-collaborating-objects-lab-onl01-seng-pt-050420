dc770900057b53e626d4566998a3684bb9c45585
Jonathan East <jonathan_east@ymail.com>
2020-05-25 23:42:36 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,6 +14,6 @@ class MP3Importer
   end
 
   def import
-    self.path.each {|file_name| Song.new_by_filename(file_name) }
+    self.files.each {|file_name| Song.new_by_filename(file_name) }
   end
 end
