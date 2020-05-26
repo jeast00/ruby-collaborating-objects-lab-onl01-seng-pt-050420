80d174273d569d1dd677ae022886c07b26c59fec
Jonathan East <jonathan_east@ymail.com>
2020-05-26 00:13:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,10 +24,10 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
-  def self.find(name)
+  def self.find_by_name(name)
     @@all.find { |artist| artist.name == name }
   end
 
